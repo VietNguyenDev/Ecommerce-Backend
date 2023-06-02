d8c113c8d33f86682150ce92a0492f96ca2fd1e7
VietNguyenDev <nguyenviet25102001@gmail.com>
2023-06-02 15:36:03 +0700

    fix favorite

--- src/http/model/favorites.model.ts
+++ src/http/model/favorites.model.ts
@@ -14,6 +14,7 @@ Favorite.init({
             type: DataTypes.INTEGER.UNSIGNED,
             allowNull: false,
             primaryKey: true,
+            autoIncrement: true
         },
         productId: {
             type: DataTypes.INTEGER.UNSIGNED,
