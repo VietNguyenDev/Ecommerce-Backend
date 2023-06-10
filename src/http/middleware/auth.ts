babf070049291f7abb81ffcdecc7f670d1cb5af6
VietNguyenDev <nguyenviet25102001@gmail.com>
2023-06-10 10:35:55 +0700

    add check permission

--- src/http/middleware/auth.ts
+++ src/http/middleware/auth.ts
@@ -1,39 +1,56 @@
+// import { Request, Response, NextFunction } from "express";
+// import {verifyToken} from "../helper/jwt";
+// import User from "../model/user.model";
+// import { JwtPayload } from "jsonwebtoken";
+
+// interface AuthenticatedRequest extends Request {
+//     user?: User;
+// }
+
+// interface Payload extends JwtPayload {
+//     id: number;
+// }
+
+// async function getUser(req: Request) {
+//     const authorization: string = req.headers.authorization || '';
+//     if (authorization === '') return false;
+//     if (!authorization.startsWith('Bearer ')) return false;
+//     const token: string = authorization.slice(7, authorization.length);
+//     const payload = await verifyToken(token) as Payload | false;
+//     if (payload === false) return false;
+
+//     const user = await User.findOne({where: { id: payload.id }});
+//     if (!user) return false;
+//     return user;
+// }
+
+// async function auth(req: AuthenticatedRequest, res: Response, next: NextFunction) {
+//     const user = await getUser(req);
+//   if (!user) {
+//     return res.status(401).send({
+//       message: 'You must be logged in',
+//     });
+//   }
+
+//     req.user = user;
+//   return next();
+// }
+
+// export { auth };
+
+import jwt from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
-import {verifyToken} from "../helper/jwt";
-import User from "../model/user.model";
-import { JwtPayload } from "jsonwebtoken";
+import { abort } from "../helper/error";
 
-interface AuthenticatedRequest extends Request {
-    user?: User;
-}
+export async function auth(req: Request, res: Response, next: NextFunction) {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+    const decoded = jwt.verify(token!, process.env.JWT_KEY!);
+    req.body.user = decoded;
+    console.log(req.body.user);
 
-interface Payload extends JwtPayload {
-    id: number;
-}
-
-async function getUser(req: Request) {
-    const authorization: string = req.headers.authorization || '';
-    if (authorization === '') return false;
-    if (!authorization.startsWith('Bearer ')) return false;
-    const token: string = authorization.slice(7, authorization.length);
-    const payload = await verifyToken(token) as Payload | false;
-    if (payload === false) return false;
-
-    const user = await User.findOne({where: { id: payload.id }});
-    if (!user) return false;
-    return user;
-}
-
-async function auth(req: AuthenticatedRequest, res: Response, next: NextFunction) {
-    const user = await getUser(req);
-  if (!user) {
-    return res.status(401).send({
-      message: 'You must be logged in',
-    });
+    next();
+  } catch(error: any) {
+    return abort(error.status, error.message)
   }
-
-    req.user = user;
-  return next();
 }
-
-export { auth };
